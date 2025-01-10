b87af735f8ecff8e4ed65d48da493e491617b619
Rohit Sattuluri <rsattulu@purdue.edu>
2025-01-10 06:45:11 -0500

    progress is updating without errors + api call is efficient

--- Main.py
+++ Main.py
@@ -1,17 +1,4 @@
-def stop_is_increasing(stops):
 
-  if(len(stops) == 1):
-    if(int(stops[0][1:3]) == int(stop_list[-1][1:3])):
-      return True
-    elif (int(stops[0][1:3]) == int(stop_list[0][1:3])):
-      return False
-
-  initial = int(stops[0][1:3])
-  for stop in stops:
-    if(int(stop[1:3]) > initial):
-      return True
-    elif (int(stop[1:3]) < initial):
-      return False
 
 from google.transit import gtfs_realtime_pb2
 import requests
@@ -49,6 +36,9 @@ response = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/
 feed.ParseFromString(response.content)
 #print(stopID_to_location)
 
+for entity in feed.entity:
+  print(entity.trip_update.trip.trip_id)
+
 while True:
   train_list = []
 
@@ -69,81 +59,30 @@ while True:
 
 
     #print(remaining_stops)
-    index_prev_stop = 0
-    for stop in stop_list:
-      if(stop == remaining_stops[0][0:3]):
-        direction = stop_is_increasing(remaining_stops)
-        if(direction):
-          index_prev_stop -=1
-          break
-        else:
-          index_prev_stop += 1
-          break
-      index_prev_stop += 1
+
 
 
     expected_time_to_next_station = remaining_stop_times[0]
-    current_time = time.time()
+
     delay = entity.trip_update.stop_time_update[0].arrival.delay
-    departure_time = 0
     #capacity =
 
-    trip_id = entity.trip_update.trip.trip_id
-    stop_times = open("/Users/rohitsattuluri/PycharmProjects/wallpaper/gtfs_subway/stop_times.txt", "r")
-    contents = stop_times.readlines()
-    for content in contents:
-        line = content.split(",")
-        if(trip_id in line[0] and line[1] == (stop_list[index_prev_stop] + remaining_stops[0][-1])):
-          departure_time = line[3]
-          break
-    if(departure_time == 0 or expected_time_to_next_station == 0):
-      continue
 
+    trip_id = entity.trip_update.trip.trip_id
     print(remaining_stops)
     print(remaining_stop_times)
-    print(stop_list[index_prev_stop])
 
-
-
-
-    ##do calculations here
-
-
-    date_str = date.today()
-
-    departure_time = datetime.strptime(f"{date_str} {departure_time}", "%Y-%m-%d %H:%M:%S")
-    departure_time = int(departure_time.timestamp())
-
-    print("deaprture timie: " + str(departure_time))
     print("arrival time: " + str(expected_time_to_next_station))
-    print("current tiime: " + str(current_time))
     print("delay time: " + str(delay))
     print("trip id" + trip_id)
     #print(entity.trip_update)
 
 
 
-    progress_ratio = (current_time - departure_time) / ((current_time - departure_time) + (expected_time_to_next_station - current_time))
-    if(progress_ratio > 1):
-      progress_ratio = 1
-    elif (progress_ratio < 0):
-      progress_ratio = 0
-    print(progress_ratio)
-    print("-"*30)
 
 
 
-
-    shape_id = ""
-    for key in tripID_to_shapeID:
-      if(trip_id in key):
-        shape_id = tripID_to_shapeID[key].strip()
-        break
-
-
-
-
-    train1 = Train(trip_id, departure_time, expected_time_to_next_station, remaining_stops, remaining_stop_times, delay, stop_list[index_prev_stop], progress_ratio, shape_id)
+    train1 = Train(trip_id, expected_time_to_next_station, remaining_stops, remaining_stop_times, delay, stop_list)
     train_list.append(train1)
 
 
@@ -151,10 +90,15 @@ while True:
   print(len(train_list))
   updateNeeded = False
   while(not updateNeeded):
-    for train in train_list:
+    index = 0
+    while(index < len(train_list)):
       time.sleep(1)
-      if(not train.update_progress()):
+      if(not train_list[index].update_progress()):
         updateNeeded = True
         print("API called again, train fnished all stops")
         break
+      if(train_list[index].validTrain == False):
+        train_list.pop(index)
+        index -= 1
 
+      index += 1
--- Train.py
+++ Train.py
@@ -1,23 +1,29 @@
 import time
-
-from Main import progress_ratio
+from datetime import datetime
+from datetime import date
 
 
 class Train:
-    def __init__(self, trip_id, departure, arrival,remaining_stops, remaining_stop_times, delay, past_station, initial_progress, shape_id):
+    def __init__(self, trip_id, arrival,remaining_stops, remaining_stop_times, delay, stop_list):
         self.trip_id = trip_id
-        self.departure = departure
+
         self.arrival = arrival
         self.remaining_stops = remaining_stops
         self.remaining_stop_times = remaining_stop_times
         self.delay = delay
-        self.past_station = past_station
-        self.initial_progress = initial_progress
-        self.shape_id = shape_id
+        self.stop_list = stop_list
+        self.validTrain = True
+        self.departure_time = self.findDepartureTime()
+
     def update_progress(self):
         current_time = time.time()
-        adjusted_departure = self.departure
+        adjusted_departure = self.departure_time
         adjusted_arrival = self.arrival
+
+        total_time = adjusted_arrival - adjusted_departure
+        elapsed_time = current_time - adjusted_departure
+        self.progress_ratio = elapsed_time / total_time
+
 
         if current_time > adjusted_arrival:
             self.progress_ratio = 1  # Train has arrived
@@ -28,10 +34,10 @@ class Train:
             return True
         else:
             # Calculate progress ratio
-            total_time = adjusted_arrival - adjusted_departure
-            elapsed_time = current_time - adjusted_departure
-            self.progress_ratio = elapsed_time / total_time
-            print(self.trip_id, self.departure, self.arrival, sep="\t")
+
+            print(self.trip_id, adjusted_departure, adjusted_arrival, sep="\t")
+            print(self.remaining_stops)
+            print(self.remaining_stop_times)
             print("current tiime" +  str(current_time))
             print("progress" + str(self.progress_ratio))
             print("-"*30)
@@ -39,30 +45,89 @@ class Train:
             return True
     def update_station(self):
         current_time = time.time()
-        adjusted_departure = self.departure
-        adjusted_arrival = self.arrival
 
-        if(len(self.remaining_stop_times) > 0):
+
+        if(len(self.remaining_stop_times) > 1):
 
             self.past_station = self.remaining_stops[0][0:3]
             self.remaining_stops.pop(0)
-            self.departure = self.remaining_stop_times.pop(0)
+            self.departure_time = self.remaining_stop_times.pop(0)
             self.arrival = self.remaining_stop_times[0]
 
-            total_time = adjusted_arrival - adjusted_departure
-            elapsed_time = current_time - adjusted_departure
+            total_time = self.arrival - self.departure_time
+            elapsed_time = current_time - self.departure_time
             self.progress_ratio = elapsed_time / total_time
-            print(self.trip_id, self.departure, self.arrival, sep="\t")
+            print(self.trip_id, self.departure_time, self.arrival, sep="\t")
+            print(self.remaining_stops)
+            print(self.remaining_stop_times)
             print("current tiime" + str(current_time))
-            print("progress" + str(self.progress_ratio))
+            print("updated_ progress" + str(self.progress_ratio))
             print("-" * 30)
 
             return True
         return False
 
+    def stop_is_increasing(self, stops):
+
+        if (len(stops) == 1):
+            if (int(stops[0][1:3]) == int(self.stop_list[-1][1:3])):
+                return True
+            elif (int(stops[0][1:3]) == int(self.stop_list[0][1:3])):
+                return False
+
+        initial = int(stops[0][1:3])
+        for stop in stops:
+            if (int(stop[1:3]) > initial):
+                return True
+            elif (int(stop[1:3]) < initial):
+                return False
+
+    def findIndexPrevStop(self):
+        index_prev_stop = 0
+        for stop in self.stop_list:
+            if (stop == self.remaining_stops[0][0:3]):
+                direction = self.stop_is_increasing(self.remaining_stops)
+                if (direction):
+                    index_prev_stop -= 1
+                    break
+                else:
+                    index_prev_stop += 1
+                    break
+            index_prev_stop += 1
+        return index_prev_stop
+
+    def findDepartureTime(self):
+        estimateddeparture = 0
+        index_prev_stop = self.findIndexPrevStop()
+        stop_times = open("/Users/rohitsattuluri/PycharmProjects/wallpaper/gtfs_subway/stop_times.txt", "r")
+        contents = stop_times.readlines()
+        for content in contents:
+            line = content.split(",")
+            if (self.trip_id in line[0] and line[1] == (self.stop_list[index_prev_stop] + self.remaining_stops[0][-1])):
+                estimateddeparture = line[3]
+                break
+        if(estimateddeparture == 0):
+            validTrain = False
+            return 0
+
+        date_str = date.today()
+
+        estimateddeparture = datetime.strptime(f"{date_str} {estimateddeparture}", "%Y-%m-%d %H:%M:%S")
+        estimateddeparture = int(estimateddeparture.timestamp())
+        return estimateddeparture
+
     def getProgress(self):
+
         return self.progress_ratio
-    def estimatedPosition(self):
+    def getShapeID(self, tripID_to_shapeID):
+        shape_id = ""
+        for key in tripID_to_shapeID:
+            if (self.trip_id in key):
+                shape_id = tripID_to_shapeID[key].strip()
+                break
+
+    def estimatedPosition(self, tripID_to_shapeID):
+        shape_id = self.getShapeID(tripID_to_shapeID)
         shapes = open("/Users/rohitsattuluri/PycharmProjects/wallpaper/gtfs_subway/shapes.txt", "r")
         shape_lines = shapes.readlines()
 
@@ -70,13 +135,8 @@ class Train:
 
         for line in shape_lines:
             l = line.split(",")
-            if (l[0] == self.shape_id):
+            if (l[0] == shape_id):
                 route_path.append(line)
 
-        index = int(progress_ratio * len(route_path))
-
-
-
-
-
+        index = int(self.progress_ratio * len(route_path))
 
