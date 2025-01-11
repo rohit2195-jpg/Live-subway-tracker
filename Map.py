4a2622d8f9f948fd33de4a64ef99d2f730d08122
Rohit Sattuluri <rsattulu@purdue.edu>
2025-01-11 08:53:12 -0500

    L line map

--- Map.py
+++ Map.py
@@ -1,28 +1,49 @@
-import json
 import folium
-# Load the GeoJSON file
-with open('Subway Lines.geojson', 'r') as f:
+import json
+
+
+lines_to_colors = {
+    "A":"blue",
+    "C":"blue",
+    "E":"blue",
+    "S":"grey",
+    "B":"orange",
+    "D":"orange",
+    "F":"orange",
+    "M":"orange",
+    "G":"lightgreen",
+    "L":"lightgrey",
+    "J":"orange",#dark orage/gold
+    "N": "yellow",
+    "Q":"yellow",
+    "R":"yellow",
+    "W":"yellow",
+    "1":"red",
+    "2":"red",
+    "3":"red",
+    "4":"green",
+    "5":"green",
+    "6":"green",
+    "7":"purple",
+}
+
+geojson_path = 'Subway Lines.geojson'
+with open(geojson_path, 'r') as f:
     data = json.load(f)
 
-l_line_routes = [feature for feature in data['features'] if feature['properties']['name'] == 'G']
+g_line_features = [feature for feature in data['features'] if feature['properties']['name'] == 'L']
 
-merged_coordinates = []
-for i in range(0,5):
-    for point in l_line_routes[i]['geometry']['coordinates']:
-        lat = point[0]
-        lon = point[1]
-        merged_coordinates.append([lon, lat])
+g_line_features = sorted(g_line_features, key=lambda x: int(x['properties']['id']))
 
+m = folium.Map(location=[40.7, -73.95], zoom_start=12, tiles="cartodb positron")
 
-print(merged_coordinates)
+coordinate_list = []
+for feature in g_line_features:
+    coordinates = feature['geometry']['coordinates']
+    new_coordinates = [[lat, lon] for lon, lat in coordinates]
+    coordinate_list.extend(new_coordinates)
+    folium.PolyLine(new_coordinates, color="green", weight=3).add_to(m)
 
-map_center = [40.7128, -74.0060]
-map_nyc = folium.Map(location=map_center, zoom_start=12)
-
-folium.PolyLine(merged_coordinates, color="green", weight=5, opacity=0.8).add_to(map_nyc)
-
-# Save the map as an HTML file
-map_nyc.save("nyc_subway_map.html")
-
-# Display the map (if running in Jupyter Notebook or similar environments)
-map_nyc
+output_path = 'nyc_subway_map.html'
+m.save(output_path)
+print(f"G-line map saved to {output_path}")
